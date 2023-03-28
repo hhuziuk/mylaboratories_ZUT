7f02cc4a245f18c0aa3df79970f16a34eb03f26b
hhuziuk <huziukwork@gmail.com>
2023-03-28 15:18:00 +0200

    pre-last commit

---
+++ cmake-build-debug/CMakeFiles/lab3.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/georgijguzuk/Desktop/lab31/forest.cpp" "CMakeFiles/lab3.dir/forest.cpp.o" "gcc" "CMakeFiles/lab3.dir/forest.cpp.o.d"
+  "/Users/georgijguzuk/Desktop/lab31/main.cpp" "CMakeFiles/lab3.dir/main.cpp.o" "gcc" "CMakeFiles/lab3.dir/main.cpp.o.d"
+  "/Users/georgijguzuk/Desktop/lab31/tree.cpp" "CMakeFiles/lab3.dir/tree.cpp.o" "gcc" "CMakeFiles/lab3.dir/tree.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
